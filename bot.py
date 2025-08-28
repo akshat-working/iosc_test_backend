b00a8806682fee8098fb1fe9814b8f9bb6b95aad
akshat-working <akshattalwar100@gmail.com>
2025-08-28 21:47:18 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ app = FastAPI(title="IoSC RAG Chatbot API", version="1.0.0")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://iosc-website-test.vercel.app/", "http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["https://iosc-website-test.vercel.app/","https://www.iosc-edc.club", "http://localhost:3000", "http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
